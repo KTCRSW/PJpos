323c4e3e965f5f02601287f1e7163b323d947829
dadaKitt <kittichdev@gmail.com>
2024-02-26 00:33:05 +0700

    Authorization

--- Backend/Controller/auth.js
+++ Backend/Controller/auth.js
@@ -1,6 +1,7 @@
 const users = require('../Model/auth.model')
 const bcrypt = require('bcrypt')
-const dotenv = require('dotenv');
+const dotenv = require('dotenv')
+const jwt = require('jsonwebtoken')
 dotenv.config();
 
 exports.register = async (req, res) => {
@@ -28,31 +29,63 @@ exports.register = async (req, res) => {
 exports.login = async (req, res) => {
     try {
         const { username, password } = req.body;
-        const getUser = users.findOne({ username: username })
+        const getUser = await users.findOne({ username: username }).exec();
 
         if (getUser) {
             const matchPassword = await bcrypt.compare(password, getUser.password);
+            const token = jwt.sign({ username: username}, process.env.SECRET, {
+                expiresIn: "1h",
+            })
             if (matchPassword) {
+                
                 res.status(200).json({
                     "success": true,
-                    "message": "Login Successfully"
+                    "message": "Login Successfully",
+                    "token": token
                 })
             } else {
                 res.status(500).json({
                     "status": false,
                     "message": "Login Failled"
                 })
+                res.json(getUser);
             }
         } else {
             res.status(500).json({
                 "status": false,
                 "message": "Login Failled"
             })
+            res.send(getUser);
         }
     } catch (error) {
         res.status(500).json({
-            "failed": false,
-            "message": "Login Failled",
+                "failed": false,
+                "message": "Login Failled",
+            })
+    }
+}
+
+function VerifyToken(req, res, next){
+    const token = req.headers["Authorization"]
+    if(typeof token !== "undefined"){
+        jwt.verify(token, process.env.SECRET, (err, authData)=>{
+            if(err){
+                res.sendStatus(403)
+            } else {
+                next();
+            }
         })
+    } else [
+        res.sendStatus(403)
+    ]
+}
+
+exports.listUser = VerifyToken, async (req, res) =>{
+    try {
+        const AllUsers = await users.find({}).exec();
+        res.json(AllUsers)
+    } catch (err) {
+        console.log(err)
+        res.status(500).send('Internal Server Error')
     }
 }
--- Backend/Routes/auth.js
+++ Backend/Routes/auth.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { register, login }  = require('../Controller/auth');
+const { register, login, listUser }  = require('../Controller/auth');
 
 
-router.post('/r', register)
-router.get('/l', login)
+router.post('/register', register)
+router.get('/login', login)
+router.get('/users', listUser)
 
 module.exports = router;
